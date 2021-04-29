972aba1e2358b4d064207024739099d530bff8fb
Alexander A. Barkan <albarkan@inbox.ru>
2021-04-30 01:06:31 +0300

    quantity.vector.0

---
+++ task0.vector.string/task1/task0.vector.odd.quantity.cpp
@@ -0,0 +1,20 @@
+﻿#include <iostream>
+#include <vector>
+// #include <string>
+int main() {
+	std::vector<int> nums = { 1, 5, 7, 10, 14 };
+	// string s = "acdefg";
+	/* for (char c : s){
+	std::cout << c << ",";
+	}
+	*/
+	int quantity = 0;
+	for (auto c : nums){	
+		if (c % 2 == 1){
+			quantity++;
+		}
+	}
+
+	std::cout << "In this vector are " << quantity << " odd numbers.\n";
+	return 0;
+}
